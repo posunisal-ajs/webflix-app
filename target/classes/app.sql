4c6b2f9c51a0c2d0bb77f37fba82ca5e4e49f8bb
unknown <sergiorobsantos@gmail.com>
2018-03-10 13:58:22 -0300

    app.sql

--- target/classes/app.sql
+++ target/classes/app.sql
@@ -15,3 +15,23 @@ CREATE TABLE usuario
   uuid character varying(150),
   CONSTRAINT usuario_pkey PRIMARY KEY (id)
 )
+
+
+CREATE SEQUENCE seqFilme
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+  
+CREATE TABLE usuario
+(
+  id bigint NOT NULL DEFAULT nextval('seqFilme'::regclass),
+  nome character varying(100),
+  genero character varying(100),
+  lancamento character varying(100),
+  informacoes character varying(255),
+  img   character varying(255),
+  uuid character varying(150),
+  CONSTRAINT usuario_pkey PRIMARY KEY (id)
+)
